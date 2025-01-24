abb906cc72bb273bc1260b7b3ecdd68cc137f9e4
Yilbat <mudnoethankgod@gmail.com>
2025-01-24 23:01:50 +0100

    structure

--- structure/programs/structure/src/lib.rs
+++ structure/programs/structure/src/lib.rs
@@ -1,16 +1,34 @@
 use anchor_lang::prelude::*;
 
-declare_id!("HC15hMijkiBo6hFZaqXLJ8xhY57nQmLjbXCbF4DyBCSF");
+declare_id!("FeJEADiL5cbnkgVzmB8Kyj3xZuPHpDF572Z8i1uRQ7gj");
 
 #[program]
-pub mod structure {
+pub mod eschrow {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
         msg!("Greetings from: {:?}", ctx.program_id);
         Ok(())
     }
-}
+
+    // buy function
+    pub fn buy(ctx: Context<Buy>, product_name: String, amount: u64) -> Result<()> {
+        Ok(())  // Signals successful transaction
+    }
+
+    // implement sell function
+    pub fn sell(ctx: Context<Sell>, product_name: String, amount: u64)-> Result<()> {
+        Ok(())  // Signals successful transaction
+    }
+    
+
+
 
 #[derive(Accounts)]
 pub struct Initialize {}
+
+#[derive(Accounts)]
+pub struct Buy {}
+
+#[derive(Accounts)]
+pub struct Sell {}
